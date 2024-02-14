cbb52092ae7a8a66ae623f9ce2de43836027dbf0
Mario <m.margjini@teamsystem.com>
2024-02-14 14:44:50 +0100

    Response update

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import express, { Request, Response } from "express";
-import { json } from "body-parser";
-import mainRoute from "./routes/mainRoute";
-import helmet from "helmet";
-import hpp from "hpp";
-//@ts-ignore   
-import { xss } from "express-xss-sanitizer";
-import cors from "cors";
+import express, { Request, Response } from "express"
+import { json } from "body-parser"
+import mainRoute from "./routes/mainRoute"
+import helmet from "helmet"
+import hpp from "hpp"
+//@ts-ignore
+import { xss } from "express-xss-sanitizer"
+import cors from "cors"
 
-const app = express();
-const port = 5000;
+const app = express()
+const port = 5000
 
 //for security
-app.use(helmet());
-app.use(hpp());
-app.use(xss());
-app.use(cors());
+app.use(helmet())
+app.use(hpp())
+app.use(xss())
+app.use(cors())
 
 // For accepting post form data
-app.use(json());
+app.use(json())
 
-app.use("/main", mainRoute);
+app.use("/main", mainRoute)
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
+  res.send("Welcome to Horizont Labs Backend Coding Task :)")
+})
 
 app.listen(port, () => {
-  console.log("Server is running on port: ", port);
-});
+  console.log("Server is running on port: ", port)
+})
